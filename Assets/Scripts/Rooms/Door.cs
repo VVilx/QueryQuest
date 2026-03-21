2104c329c7588ae52d0288bca7338041d67b62aa
Areesh613 <achowdh17@gmail.com>
2026-03-21 18:07:19 -0400

    Update the asset tags level 1

--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -6,10 +6,23 @@ public class Door : MonoBehaviour
     [SerializeField] private Transform nextRoom;
     [SerializeField] private CameraController cam;
 
+    [SerializeField] private QuizTrigger quiz;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            if (quiz != null && !quiz.completed)
+            {
+                if (collision.transform.position.x < transform.position.x)
+                    collision.transform.position = new Vector3(transform.position.x - 1f, collision.transform.position.y, collision.transform.position.z);
+                else
+                    collision.transform.position = new Vector3(transform.position.x + 1f, collision.transform.position.y, collision.transform.position.z);
+                    
+                Debug.Log("Finish the quiz before entering!");
+                return;    
+            }
+
             if (collision.transform.position.x < transform.position.x)
             {
                 cam.MoveToNewRoom(nextRoom);
