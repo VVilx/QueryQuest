b3717c8c723f3cae9369353a14cae9698162851e
Areesh613 <achowdh17@gmail.com>
2026-04-07 13:48:05 -0400

    Updated Level 10

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -149,7 +149,7 @@ public class PlayerMovement : MonoBehaviour
 
     public bool canAttack()
     {
-        return horizontalInput == 0 && isGrounded() && !onWall();
+        return horizontalInput == 0 && !onWall();
     }     
 }
 
